e30cc7905559e6b6dcf80af4c901437d92cabd92
Andrei Popa <andrei@hashicorp.com>
2020-08-05 17:21:44 +0200

    add example

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "remote" {
+    hostname     = "ptfe.int.radacina.xyz"
+    organization = "frankfurt-hq"
+
+    workspaces {
+      name = "tags-data-test"
+    }
+  }
+}
+
+data "external" "git_commit_author" {
+  program     = ["/bin/bash", "-c", "echo \"{\\\"git_commit_author\\\":\\\"$(git log -1 | grep ^Author | cut -f 2 -d : | sed 's/^ //g')\\\"}\""]
+  working_dir = path.root
+}
