e2e5feb1e20bece85401dc4c68bc04a5fccf470d
Andrei Popa <38042793+andrewpopa@users.noreply.github.com>
2020-08-05 17:28:02 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ terraform {
   }
 }
 
+
 data "external" "git_commit_author" {
   program     = ["/bin/bash", "-c", "echo \"{\\\"git_commit_author\\\":\\\"$(git log -1 | grep ^Author | cut -f 2 -d : | sed 's/^ //g')\\\"}\""]
   working_dir = path.root
